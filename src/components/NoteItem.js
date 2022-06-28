29e594e7d3dd9a0709f858c52f5171af74317212
AlmightyAmogh <amogh2020@gmail.com>
2022-06-28 14:16:42 +0530

    added button to noteitem for delete and edit

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -5,14 +5,16 @@ const NoteItem = (props) => {
   const { note } = props;
   return (
     <div className="col-md-3">
-      <div class="card my-3">
-        <div class="card-body">
-          <h5 class="card-title">{note.title}</h5>
-          <p class="card-text">{note.description}</p>
-          <i class="fa fa-solid fa-trash-can mx-2"></i>
-          <i class="fa fa-solid fa-pen-to-square mx-2"></i>
-          <p>Delete</p>
-          <p>Edit</p>
+      <div className="card my-3">
+        <div className="card-body">
+          <h5 className="card-title">{note.title}</h5>
+          <p className="card-text">{note.description}</p>
+          {/* <i className="fa fa-solid fa-trash-can mx-2"></i>
+          <i className="fa fa-solid fa-pen-to-square mx-2"></i> */}
+          <div className="d-flex align-items-center">
+          <button type="button" className="btn btn-primary mx-3">Delete</button>
+          <button type="button" className="btn btn-primary mx-3">Edit</button>
+          </div>
         </div>
       </div>
     </div>
